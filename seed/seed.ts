712c6cf7f65f5f045b11bc190391d3f6c6aea6fa
Ronaldo Alencar <ronaldom32@gmail.com>
2024-01-10 09:30:11 -0300

    feat: added index in column to improve perfomance

--- seed/seed.ts
+++ seed/seed.ts
@@ -15,7 +15,8 @@ async function seed() {
       title VARCHAR(255) NOT NULL,
       link VARCHAR(255) NOT NULL,
       posted_at VARCHAR(255) NOT NULL,
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      INDEX (link)
     )
   `);
 
