09a221c54da0e96333362f19b4c3b88b208dc8fa
Ronaldo <ronaldom32@gmail.com>
2024-01-09 23:01:30 -0300

    feat: added column created_at in database

--- seed/seed.ts
+++ seed/seed.ts
@@ -14,7 +14,8 @@ async function seed() {
       id INT AUTO_INCREMENT PRIMARY KEY,
       title VARCHAR(255) NOT NULL,
       link VARCHAR(255) NOT NULL,
-      posted_at VARCHAR(255) NOT NULL
+      posted_at VARCHAR(255) NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )
   `);
 
